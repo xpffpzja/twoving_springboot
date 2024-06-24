7ccb0bbf2e7b03958bca3a028d4ce35abe8014af
xpffpzja <xpffpzja@gmail.com>
2024-06-24 18:16:23 +0900

    20240624 commit 1

---
+++ twoving/src/main/java/com/himedia/twoving/TwovingApplication.java
@@ -0,0 +1,13 @@
+package com.himedia.twoving;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwovingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwovingApplication.class, args);
+    }
+
+}
